761e594a8f8395160599de126a87140409f6739c
Roger <ranguera@gmail.com>
2017-02-08 16:49:39 -0800

    pathfinder0.1

---
+++ Assets/Pathfinder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour {
+
+    public Transform player;
+    public Transform maze;
+
+    private int x, z;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        x = Mathf.RoundToInt(player.position.x / maze.localScale.x);
+        z = Mathf.RoundToInt(player.position.z / maze.localScale.y);
+        print("pos: " + x + " , " + z);
+	}
+}
